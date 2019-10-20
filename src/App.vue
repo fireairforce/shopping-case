cfb7ca7456a2f91db67d1c5963b57506880ea5fe
fireairforce <1344492820@qq.com>
2019-10-20 13:54:33 +0800

    feat:按需引入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,95 @@
 <template>
   <div id="app">
-    <h1>{{ title }}</h1>
-    <hr />
     <div>
-      <h2>添加课程</h2>
-      <div>
-        <label for>课程名称:</label>
-        <input type="text" v-model="courseInfo.name" />
-      </div>
-      <div>
-        <label for>课程价格:</label>
-        <!-- 这里使用v-model来做一个数据的双向绑定,把数组动态双向绑定到course里面 -->
-        <input type="text" v-model="courseInfo.price" />
-      </div>
-      <div>
-        <button @click="addCourseToList">添加课程到列表</button>
-      </div>
+      <h3>Element表单</h3>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="用户名" prop="pass">
+          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+      <hr />
     </div>
-    <dir>
-      <h2>课程列表</h2>
-      <table>
-        <tr>
-          <th>课程名称</th>
-          <th>课程价格</th>
-          <th>操作</th>
-        </tr>
-        <tr v-for="(item, index) in courseList" :key="item.id">
-          <td>{{item.name}}</td>
-          <td>{{item.price}}</td>
-          <td>
-            <button @click="addCourseToCart(index)">添加到购物车</button>
-          </td>
-        </tr>
-      </table>
-    </dir>
-    <cart :courseItem="courseItem" @removeItem="remove" @minusItem="minusItem" @addItem="addItem" />
   </div>
 </template>
 
 <script>
-import cart from "./components/Cart";
 export default {
   name: "app",
-  components: {
-    cart
-  },
-  methods: {
-    addCourseToList() {
-      this.courseList.push(this.courseInfo);
-    },
-    addCourseToCart(index) {
-      let item = this.courseList[index];
-      let isHasCourse = this.courseItem.find(x => x.id === item.id);
-      if (isHasCourse) {
-        isHasCourse.number++;
-      } else {
-        this.courseItem.push({ ...item, number: 1, isActive: false });
-      }
-    },
-    remove(index) {
-      this.courseItem.splice(index, 1);
-    },
-    minusItem(index) {
-      this.courseItem[index].number--;
-    },
-    addItem(index) {
-      this.courseItem[index].number++;
-    }
-  },
+  components: {},
   data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
-      title: "购物车",
-      courseInfo: {
-        name: "",
-        price: ""
+      ruleForm: {
+        pass: "",
+        checkPass: ""
       },
-      courseItem: [],
-      courseList: [
-        { name: "计算机网络", price: 9999, id: 1 },
-        { name: "深度学习", price: 8888, id: 2 }
-      ]
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+      },
+      methods: {
+        submitForm(formName) {
+          this.$refs[formName].validate(valid => {
+            if (valid) {
+              alert("submit!");
+            } else {
+              console.log("error submit!!");
+              return false;
+            }
+          });
+        },
+        resetForm(formName) {
+          this.$refs[formName].resetFields();
+        }
+      }
     };
   }
 };
